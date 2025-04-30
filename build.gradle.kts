352645cc3de23130e3aa0f2d4d978893294c97ed
fayust <voldores@mail.ru>
2025-04-30 22:46:30 +0300

    add checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.10"
+    id("checkstyle")
     application
 }
 
-group = "org.example"
+group = "ru.job4j"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
@@ -20,12 +22,30 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+kotlin {
+    jvmToolchain {
+        (this).languageVersion.set(JavaLanguageVersion.of(17)) // версия Java 17
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17" // версия JVM для Kotlin
+    }
+}
+
+checkstyle {
+    toolVersion = "8.45"
+    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
+}
+
+
 tasks.test {
     useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
 
 application {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "job4j_kafka"
+rootProject.name = "KafkaCourse"
 
